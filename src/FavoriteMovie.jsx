ae991ae5f83458433063dcb85c5cd338e3e29ee8
Hélène Baudrand <baudrand.helene@gmail.com>
2019-11-16 18:27:04 +0100

    form before fetch

--- src/FavoriteMovie.jsx
+++ src/FavoriteMovie.jsx
@@ -7,27 +7,60 @@ class FavoriteMovie extends Component {
       movieName : '',
       poster: '',
       comment: ''
-     };
+  };
+  this.onChange = this.onChange.bind(this)
+  this.submitForm = this.submitForm.bind(this)
   }
+  onChange(e) {
+    this.setState({
+      [e.target.movieName] : e.target.value
+    });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <div className="favorite-movie">
-        <form>
+      <div className="favoriteMovie">
+        <form onSubmit={this.submitForm}>
           <fieldset>
-            <div>
-              <label>
-                <input />
+            <div className="form-data">
+              <label htmlFor="movieName">
+                <input
+                type="text"
+                id="movieName"
+                name="movieName"
+                onChange={this.onChange}
+                value={this.state.movieName} 
+                />
               </label>
             </div>
-            <div>
-              <label>
-                <input />
+            <div className="form-data">
+              <label htmlFor="comment">
+                <input
+                type="text"
+                id="poster"
+                name="poster"
+                onChange={this.onChange}
+                value={this.state.poster} 
+                />
+              </label>
+            </div >
+            <div className="form-data">
+              <label label htmlFor="comment">
+                <input
+                type="textarea"
+                id="comment"
+                name="comment"
+                onChange={this.onChange}
+                value={this.state.comment} 
+                />
               </label>
             </div>
-            <div>
-              <label>
-                <input />
-              </label>
+            <div className="form-data">
+              <input type="submit" value="send"></input>
             </div>
           </fieldset>
         </form>
